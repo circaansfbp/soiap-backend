49e9fc5bf5be797bd17cf84f6e9ed0ab510457ea
circaansfbp <franciscambp@outlook.com>
2021-04-01 11:16:34 -0300

    backend - proyecto base

---
+++ src/main/java/pt/fmbp/soiapbackend/SoiapBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.fmbp.soiapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoiapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoiapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/fmbp/soiapbackend/SoiapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.fmbp.soiapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoiapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
